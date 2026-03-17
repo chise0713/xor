fb4db50b2bb21568268a0eacef26176a3ea5555c
チセ <123655015+chise0713@users.noreply.github.com>
2026-03-17 19:56:13 +0800

    Refactor: Bitwise tail decomposition

--- src/methods/xor.rs
+++ src/methods/xor.rs
@@ -1,37 +1,32 @@
-use std::{slice, sync::OnceLock};
+use core::slice;
+use std::sync::OnceLock;
 
 use anyhow::Result;
-use wide::u64x8;
+use wide::{u64x4, u64x8};
 
 use super::{ApplyProof, MethodApply};
-use crate::{INIT, ONCE, buf_pool::SIMD_WIDTH, const_concat};
+use crate::{INIT, ONCE, const_concat};
 
-static TOKEN_U8: OnceLock<u8> = OnceLock::new();
-static TOKEN_SIMD: OnceLock<u64x8> = OnceLock::new();
+static TOKEN: OnceLock<u8> = OnceLock::new();
 
 pub struct XorToken;
 
 impl XorToken {
-    const BROADCAST_MUL: u64 = 0x0101010101010101;
     pub fn init(val: u8) -> Result<()> {
         const_concat! {
             CTX = "XorToken::set()" + ONCE
         };
-        TOKEN_U8.set(val).expect(&CTX);
-
-        TOKEN_SIMD
-            .set(u64x8::splat(Self::BROADCAST_MUL.wrapping_mul(val as u64)))
-            .expect(&CTX);
+        TOKEN.set(val).expect(&CTX);
         Ok(())
     }
 
     #[must_use]
     #[inline(always)]
-    fn get() -> (u8, u64x8) {
+    fn get() -> u8 {
         const_concat! {
             CTX = "XorToken::get()" + INIT
         };
-        (*TOKEN_U8.get().expect(&CTX), *TOKEN_SIMD.get().expect(&CTX))
+        *TOKEN.get().expect(&CTX)
     }
 }
 
@@ -63,8 +58,8 @@ impl MethodApply for Xor {
     where
         P: super::ApplyProof<Method = Self>,
     {
-        let (token, simd) = XorToken::get();
-        unsafe { xor(ptr, *n, token, simd) }
+        let token = XorToken::get();
+        unsafe { xor(ptr, *n, token) }
     }
 
     #[inline(always)]
@@ -76,25 +71,53 @@ impl MethodApply for Xor {
 }
 
 #[inline(always)]
-unsafe fn xor(ptr: *mut u8, n: usize, token: u8, simd: u64x8) {
+unsafe fn xor(ptr: *mut u8, n: usize, token: u8) {
     super::align_check(ptr.addr());
+
     if token == 0 {
         return;
     }
 
-    let n_simd = n / SIMD_WIDTH;
-    let data: &mut [u64x8] = unsafe { slice::from_raw_parts_mut(ptr.cast(), n_simd) };
+    let t128 = u128::from_ne_bytes([token; _]);
+    let t64 = u64::from_ne_bytes([token; _]);
+    let t32 = u32::from_ne_bytes([token; _]);
+    let t16 = u16::from_ne_bytes([token; _]);
 
-    data.iter_mut().for_each(|chunk| *chunk ^= simd);
+    let simd8 = u64x8::splat(t64);
 
-    let rem = n % SIMD_WIDTH;
-    if rem == 0 {
-        return;
+    const CHUNK_SIZE: usize = size_of::<u64x8>();
+
+    let chunks = n / CHUNK_SIZE;
+
+    if chunks != 0 {
+        let data: &mut [u64x8] = unsafe { slice::from_raw_parts_mut(ptr.cast(), chunks) };
+
+        data.iter_mut().for_each(|chunk| *chunk ^= simd8);
     }
-    let rem_ptr = unsafe { ptr.add(n_simd * SIMD_WIDTH) };
 
-    let tail = unsafe { slice::from_raw_parts_mut(rem_ptr, rem) };
-    tail.iter_mut().for_each(|byte| *byte ^= token);
+    let base = unsafe { ptr.add(chunks * CHUNK_SIZE) };
+    let mut off = 0;
+    let tail = n - chunks * CHUNK_SIZE;
+
+    // branchless tail
+    macro_rules! xor_tail {
+        ($size:expr, $t:ty, $token:expr) => {
+            if tail & $size != 0 {
+                let v: *mut $t = unsafe { base.add(off) }.cast();
+                unsafe { *v ^= $token };
+                off += $size;
+            }
+        };
+    }
+
+    xor_tail!(size_of::<u64x4>(), u64x4, u64x4::splat(t64));
+    xor_tail!(size_of::<u128>(), u128, t128);
+    xor_tail!(size_of::<u64>(), u64, t64);
+    xor_tail!(size_of::<u32>(), u32, t32);
+    xor_tail!(size_of::<u16>(), u16, t16);
+    xor_tail!(size_of::<u8>(), u8, token);
+
+    _ = off;
 }
 
 #[test]
@@ -133,9 +156,7 @@ mod bench {
     extern crate test;
 
     use test::Bencher;
-    use wide::u64x8;
 
-    use super::XorToken;
     use crate::{K, buf_pool::AlignBox};
 
     const N: usize = 16 * K;
@@ -145,14 +166,7 @@ mod bench {
     fn simd(b: &mut Bencher) {
         let mut data = AlignBox::new(N);
         let ptr = data.as_mut_ptr();
-        b.iter(|| unsafe {
-            super::xor(
-                ptr,
-                N,
-                TOKEN,
-                u64x8::splat(XorToken::BROADCAST_MUL.wrapping_mul(TOKEN as u64)),
-            )
-        });
+        b.iter(|| unsafe { super::xor(ptr, N, TOKEN) });
     }
 
     #[bench]
