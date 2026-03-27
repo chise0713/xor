6f5bb0034b7611d521b63e31c124031eba586f7c
チセ <123655015+chise0713@users.noreply.github.com>
2026-03-27 23:10:57 +0800

    Fix: Enforce invariants with `assert!`

--- src/buf_pool.rs
+++ src/buf_pool.rs
@@ -162,7 +162,7 @@ mod sealed {
             {
                 let mut v = self.available.lock();
 
-                debug_assert!(v.len() < v.capacity());
+                assert!(v.len() < v.capacity());
 
                 v.push(meta);
             }
@@ -184,7 +184,7 @@ mod sealed {
         pub(super) unsafe fn meta_to_buf(&self, meta: usize) -> LeasedBuf {
             let offset = meta * self.stride;
 
-            debug_assert!(offset < self.slab.padded_len);
+            assert!(offset < self.slab.padded_len);
 
             let ptr = ptr::slice_from_raw_parts_mut(
                 unsafe { self.slab.ptr.as_ptr().add(offset) },
